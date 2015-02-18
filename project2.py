86104a723682d8f2ec8f975d26fe54c22aa9d8d5
mhd1991 <m91.alahmadi@gmail.com>
2015-02-18 13:28:43 -0800

    updated again

--- project2.py
+++ project2.py
@@ -94,4 +94,4 @@ if __name__ == '__main__':
 	# 		pprint(dat, f)
 	# 		f.write('\n-------------------------------------------------------\n\n')
 
-	top_5_tweets('##GoPatriots')
+	top_5_tweets('#GoPatriots')
